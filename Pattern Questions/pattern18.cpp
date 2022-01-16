8e14c02231b1b11b74f1d31df00b161a83a16495
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2022-01-16 23:34:55 +0530

    Pattern Based Question

---
+++ Pattern Questions/pattern18.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+        
+           while(j<i){
+                cout<<" ";
+                j++;
+               
+            } 
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+      }
+    return 0;
+}
+/* INPUT 
+4
+OUTPUT 
+****
+ ***
+  **
+   *     */
